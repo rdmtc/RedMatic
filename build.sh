2c66f50b2a3c4b422c6f5c13d06d30ede8cf037f
hobbyquaker <bastiraff@googlemail.com>
2018-04-29 21:14:32 +0200

    auto-generate changelog for release page on travis, cleanup build.sh

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ ADDON_TMP=$BUILD_DIR/addon_tmp
 
 mkdir $ADDON_TMP 2> /dev/null || rm -r $ADDON_TMP/*
 
+
 echo "download and extract Node.js $NODE_URL ..."
 curl --silent $NODE_URL | tar -xJf - -C $ADDON_TMP
 mv $ADDON_TMP/$NODE_NAME $ADDON_TMP/node-red
@@ -20,13 +21,16 @@ rm $ADDON_TMP/node-red/README.md
 rm $ADDON_TMP/node-red/CHANGELOG.md
 mv $ADDON_TMP/node-red/LICENSE $ADDON_TMP/node-red/LICENSE_Nodejs
 
+
 echo "copying files to tmp dir..."
 cp -r $ADDON_FILES/* $ADDON_TMP/
 
+
+echo "installing node modules..."
 cp package.json $ADDON_TMP/node-red/
 cd $ADDON_TMP/node-red
-echo "installing node modules..."
 npm install --silent --no-package-lock --production --no-optional --global-style
+
 
 echo "adapt Node-RED..."
 rm -r $ADDON_TMP/node-red/node_modules/node-red/nodes/core/hardware
@@ -36,29 +40,54 @@ mv $ADDON_TMP/node-red/node_modules/node-red/red/runtime/log.js $ADDON_TMP/node-
 sed "s/util\.log/console.log/g" $ADDON_TMP/node-red/node_modules/node-red/red/runtime/log.js.orig > $ADDON_TMP/node-red/node_modules/node-red/red/runtime/log.js
 
 
-
 echo "moving node modules to lib dir..."
 cp -r $ADDON_TMP/node-red/node_modules $ADDON_TMP/node-red/lib/
 rm -r $ADDON_TMP/node-red/node_modules
 
 cd $BUILD_DIR
 
+
 echo "creating version file"
 ADDON_VERSION=`jq -r '.version' $ADDON_TMP/node-red/package.json`
-echo "export ADDON_VERSION=$ADDON_VERSION" > $ADDON_TMP/node-red/versions
-echo "export NODE_VERSION=$NODE_VERSION" >> $ADDON_TMP/node-red/versions
-echo "export NPM_VERSION=`jq -r '.version' $ADDON_TMP/node-red/lib/node_modules/npm/package.json`" >> $ADDON_TMP/node-red/versions
-echo "export RED_VERSION=`jq -r '.version' $ADDON_TMP/node-red/lib/node_modules/node-red/package.json`" >> $ADDON_TMP/node-red/versions
-echo "export DASHBOARD_VERSION=`jq -r '.version' $ADDON_TMP/node-red/lib/node_modules/node-red-dashboard/package.json`" >> $ADDON_TMP/node-red/versions
-echo "export RED_CCU_VERSION=`jq -r '.version' $ADDON_TMP/node-red/lib/node_modules/node-red-contrib-ccu/package.json`" >> $ADDON_TMP/node-red/versions
+NPM_VERSION=`jq -r '.version' $ADDON_TMP/node-red/lib/node_modules/npm/package.json`
+RED_VERSION=`jq -r '.version' $ADDON_TMP/node-red/lib/node_modules/node-red/package.json`
+DASHBOARD_VERSION=`jq -r '.version' $ADDON_TMP/node-red/lib/node_modules/node-red-dashboard/package.json`
+RED_CCU_VERSION=`jq -r '.version' $ADDON_TMP/node-red/lib/node_modules/node-red-contrib-ccu/package.json`
 
-cat $ADDON_TMP/node-red/versions
+cat >$ADDON_TMP/node-red/versions <<EOL
+export ADDON_VERSION=$ADDON_VERSION
+export NODE_VERSION=$NODE_VERSION
+export NPM_VERSION=$NPM_VERSION
+export RED_VERSION=$RED_VERSION
+export DASHBOARD_VERSION=$DASHBOARD_VERSION
+export RED_CCU_VERSION=$RED_CCU_VERSION
+EOL
 
-ADDON_FILE=ccu-addon-node-red-$ADDON_VERSION.tar.gz
+
+echo "creating changelog file"
+cat >CHANGELOG.md <<EOL
+### Module Versions
+
+* Node.js $NODE_VERSION
+* npm $NPM_VERSION
+* Node-RED $RED_VERSION
+* Node-RED Dashboard $DASHBOARD_VERSION
+* node-red-contrib-ccu $RED_CCU_VERSION
+
+
+### Changelog
+
+EOL
+
+git log `git describe --tags --abbrev=0`..HEAD --pretty=format:'* %h @%an %s' >> CHANGELOG.md
+
+
 echo "compressing addon package $ADDON_FILE ..."
+ADDON_FILE=ccu-addon-node-red-$ADDON_VERSION.tar.gz
 mkdir $BUILD_DIR/dist 2> /dev/null
 cd $ADDON_TMP
 tar --exclude=.DS_Store -czf $BUILD_DIR/dist/$ADDON_FILE *
 cd $BUILD_DIR
+
 
 echo "done."
