27d2c5fea844454aa3f554fb301ccf317ced3a08
hobbyquaker <bastiraff@googlemail.com>
2018-04-30 23:33:28 +0200

    addon update check

---
+++ addon_files/node-red/update_check.tcl
@@ -0,0 +1,27 @@
+#!/usr/bin/env tclsh
+
+set checkURL    "https://api.github.com/repos/hobbyquaker/ccu-addon-node-red/releases/latest"
+set downloadURL "https://github.com/hobbyquaker/ccu-addon-node-red/releases/latest"
+
+catch {
+  set input $env(QUERY_STRING)
+  set pairs [split $input &]
+  foreach pair $pairs {
+    if {0 != [regexp "^(\[^=]*)=(.*)$" $pair dummy varname val]} {
+      set $varname $val
+    }
+  }
+}
+
+if { [info exists cmd ] && $cmd == "download"} {
+  puts "<html><head><meta http-equiv='refresh' content='0; url=$downloadURL' /></head></html>"
+} else {
+  catch {
+    [regexp "tag_name\": \"v(\[0-9\]+\.\[0-9\]+\.\[0-9\]+)" [ exec /usr/bin/env wget -qO- --no-check-certificate $checkURL ] dummy newversion]
+  }
+  if { [info exists newversion] } {
+    puts -nonewline $newversion
+  } else {
+    puts -nonewline "n/a"
+  }
+}
