574071b35dea3d26821c3bddb9605a8786ca6fc3
hobbyquaker <bastiraff@googlemail.com>
2018-05-05 17:26:17 +0200

    auto create versions file from all installed modules

--- build.sh
+++ build.sh
@@ -42,32 +42,61 @@ sed "s/util\.log/console.log/g" $ADDON_TMP/node-red/lib/node_modules/node-red/re
 cd $BUILD_DIR
 
 
-echo "creating version file"
-ADDON_VERSION=`jq -r '.version' package.json`
-NPM_VERSION=`jq -r '.version' $ADDON_TMP/node-red/lib/node_modules/npm/package.json`
+echo "creating version files"
+MODULES_DIR=$ADDON_TMP/node-red/lib/node_modules
+VERSION_FILE=$ADDON_TMP/node-red/versions
+VERSION_ADDON=`jq -r '.version' package.json`
+
+cat > $VERSION_FILE <<EOL
+export NODE_VERSION=$NODE_VERSION
+export VERSION_ADDON=$VERSION_ADDON
+EOL
+
+cat >CHANGELOG.md <<EOL
+Module | Version
+------ | -------
+EOL
+
+links() {
+    case $1 in
+        'npm')
+            URL=https://github.com/npm/npm/releases
+            ;;
+        'node-red')
+            URL=https://nodered.org/
+            ;;
+        'node-red-dashboard')
+            URL=https://github.com/node-red/node-red-dashboard/releases
+            ;;
+        'node-red-contrib-ccu')
+            URL=https://github.com/hobbyquaker/node-red-contrib-ccu
+            ;;
+        *)
+            URL=
+    esac
+
+    echo "export VERSION_`echo $1 | sed -e 's/-//g'`=$2" >> $VERSION_FILE
+
+    if [ $URL ]; then
+        echo "[$URL]($1) | $2" >> CHANGELOG.md
+    else
+        echo "$1 | $2" >> CHANGELOG.md
+    fi
+}
+
+for DIR in $(find $MODULES_DIR/ -type d -maxdepth 1 -not -name "node_modules" -not -name ".bin"  -exec basename {} \; | sort -t '\0' -n)
+do
+    VERSION=$(jq -r '.version' $MODULES_DIR/$DIR/package.json)
+    links $DIR $VERSION
+done
+
 RED_VERSION=`jq -r '.version' $ADDON_TMP/node-red/lib/node_modules/node-red/package.json`
 DASHBOARD_VERSION=`jq -r '.version' $ADDON_TMP/node-red/lib/node_modules/node-red-dashboard/package.json`
 RED_CCU_VERSION=`jq -r '.version' $ADDON_TMP/node-red/lib/node_modules/node-red-contrib-ccu/package.json`
 
-cat >$ADDON_TMP/node-red/versions <<EOL
-export ADDON_VERSION=$ADDON_VERSION
-export NODE_VERSION=$NODE_VERSION
-export NPM_VERSION=$NPM_VERSION
-export RED_VERSION=$RED_VERSION
-export DASHBOARD_VERSION=$DASHBOARD_VERSION
-export RED_CCU_VERSION=$RED_CCU_VERSION
-EOL
-
 
 echo "creating changelog file"
-cat >CHANGELOG.md <<EOL
-### Module Versions
-
-* Node.js $NODE_VERSION
-* npm $NPM_VERSION
-* Node-RED $RED_VERSION
-* Node-RED Dashboard $DASHBOARD_VERSION
-* node-red-contrib-ccu $RED_CCU_VERSION
+cat >>CHANGELOG.md <<EOL
 
 
 ### Changelog
@@ -78,7 +107,7 @@ git log `git describe --tags --abbrev=0`..HEAD --pretty=format:'* %h @%an %s' >>
 
 
 echo "compressing addon package $ADDON_FILE ..."
-ADDON_FILE=ccu-addon-node-red-$ADDON_VERSION.tar.gz
+ADDON_FILE=ccu-addon-node-red-$VERSION_ADDON.tar.gz
 mkdir $BUILD_DIR/dist 2> /dev/null
 cd $ADDON_TMP
 tar --exclude=.DS_Store -czf $BUILD_DIR/dist/$ADDON_FILE *
