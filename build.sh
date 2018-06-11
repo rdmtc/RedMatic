d8d93be7434df3cedc9d344f9ea9920390026547
hobbyquaker <bastiraff@googlemail.com>
2018-06-11 17:59:59 +0200

    fix addon version

--- build.sh
+++ build.sh
@@ -55,7 +55,7 @@ cd $BUILD_DIR
 echo "creating version files"
 MODULES_DIR=$ADDON_TMP/redmatic/lib/node_modules
 VERSION_FILE=$ADDON_TMP/redmatic/versions
-VERSION_ADDON=`jq -r '.version' $ADDON_TMP/redmatic/lib/package.json`
+VERSION_ADDON=`jq -r '.version' package.json`
 RED_VERSION=`jq -r '.version' $ADDON_TMP/redmatic/lib/node_modules/node-red/package.json`
 
 cat > $VERSION_FILE <<EOL
