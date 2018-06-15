327970f2da93ecf17c3f5fde5e62547c630675da
hobbyquaker <bastiraff@googlemail.com>
2018-06-15 23:04:09 +0200

    update lib/package.json

--- update_deps.sh
+++ update_deps.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+BUILD_DIR=`cd ${0%/*} && pwd -P`
+
+cd $BUILD_DIR/addon_files/redmatic/lib
 ncu -u
 
-cd addon_files/redmatic/var
+cd $BUILD_DIR/addon_files/redmatic/var
 ncu -u
 
-cd ../www
+cd $BUILD_DIR/addon_files/redmatic/www
 ncu -u
 
-cd ../../../
-
+cd $BUILD_DIR
 node combine_package.js
