6d4908d57f8608ffd31ae5d7e51c9f21d25b15da
hobbyquaker <bastiraff@googlemail.com>
2018-06-21 23:27:35 +0200

    use local ncu, update readme

--- update.sh
+++ update.sh
@@ -3,13 +3,14 @@
 BUILD_DIR=`cd ${0%/*} && pwd -P`
 
 cd $BUILD_DIR/addon_files/redmatic/lib
-ncu -u
+$BUILD_DIR/node_modules/.bin/ncu -u
 
 cd $BUILD_DIR/addon_files/redmatic/var
-ncu -u
+$BUILD_DIR/node_modules/.bin/ncu -u
 
 cd $BUILD_DIR/addon_files/redmatic/www
-ncu -u
+$BUILD_DIR/node_modules/.bin/ncu -u
 
 cd $BUILD_DIR
-node combine_package.js
+node update_package.js
+node update_readme.js
