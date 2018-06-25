10b8c1f5fc3678125397fb4c3ad7f059c932dece
hobbyquaker <bastiraff@googlemail.com>
2018-06-25 19:11:43 +0200

    fix changelog

--- build.sh
+++ build.sh
@@ -130,7 +130,7 @@ do
     links $DIR $VERSION
 done
 
-echo "\n[Travis Build #$TRAVIS_BUILD_NUMBER](https://travis-ci.org/hobbyquaker/RedMatic/builds/$TRAVIS_BUILD_ID)" >> CHANGELOG.md
+echo -e "\n\n[Travis Build #$TRAVIS_BUILD_NUMBER](https://travis-ci.org/hobbyquaker/RedMatic/builds/$TRAVIS_BUILD_ID)" >> CHANGELOG.md
 
 
 echo "compressing addon package $ADDON_FILE ..."
