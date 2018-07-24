46598c498bb0c2a9204d625e2fe1360f90b160d4
hobbyquaker <bastiraff@googlemail.com>
2018-07-24 19:57:42 +0200

    dl badge link

--- build.sh
+++ build.sh
@@ -65,7 +65,7 @@ EOL
 
 echo "creating changelog file"
 cat >CHANGELOG.md <<EOL
-![](https://img.shields.io/github/downloads/hobbyquaker/RedMatic/v$VERSION_ADDON/total.svg)
+[![](https://img.shields.io/github/downloads/hobbyquaker/RedMatic/v$VERSION_ADDON/total.svg)](https://github.com/hobbyquaker/RedMatic/releases/download/v$VERSION_ADDON/redmatic-$VERSION_ADDON.tar.gz)
 
 ### Changelog
 
