539076dcda4887bd90224b94eb12f207283fb430
hobbyquaker <bastiraff@googlemail.com>
2019-02-17 20:11:39 +0100

    Update node-red-contrib-ccu, RedMatic-HomeKit, node-red-node-email, node-red-node-serialport, node-red-contrib-sun-position, npm

--- prebuild.sh
+++ prebuild.sh
@@ -8,7 +8,7 @@ cat addon_files/redmatic/lib/package.json | jq 'del(.dependencies.npm,.dependenc
 
 scp $DEST/package.json $REMOTE:$REMOTE_PATH
 
-#ssh -t $REMOTE "cd $REMOTE_PATH ; npm install --global-style"
+ssh -t $REMOTE "cd $REMOTE_PATH ; npm install --global-style"
 
 rm -r ${DEST}/lib/node_modules
 
