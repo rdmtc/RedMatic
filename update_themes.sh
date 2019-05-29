349717fa3559915086f3146432b76a545b0795cf
hobbyquaker <bastiraff@googlemail.com>
2019-05-29 22:58:56 +0200

    fix theme update

--- update_themes.sh
+++ update_themes.sh
@@ -5,7 +5,7 @@ THEMES_DIR=$BUILD_DIR/addon_files/redmatic/lib/node-red-themes
 
 echo "update theme bonanitech/midnight-red"
 mkdir -p $THEMES_DIR/midnight-red
-curl --silent --show-error -o $THEMES_DIR/midnight-red/midnight.css https://raw.githubusercontent.com/rdmtc/midnight-red/master/midnight.css
-curl --silent --show-error -o $THEMES_DIR/midnight-red/theme-tomorrow.js https://github.com/rdmtc/midnight-red/blob/master/theme-tomorrow.js
+curl --location --silent --show-error -o $THEMES_DIR/midnight-red/midnight.css https://raw.githubusercontent.com/rdmtc/midnight-red/master/midnight.css
+curl --location --silent --show-error -o $THEMES_DIR/midnight-red/theme-tomorrow.js  https://raw.githubusercontent.com/rdmtc/midnight-red/master/theme-tomorrow.js
 
 git add $THEMES_DIR
