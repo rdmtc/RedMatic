14ded26ccb41a088f3cef0ab3312a24dbb46aa04
hobbyquaker <bastiraff@googlemail.com>
2020-07-09 18:24:59 +0200

    reorganize prebuild scripts

--- prebuild_armv6l.sh
+++ prebuild_armv6l.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DEST="prebuilt/i686"
-REMOTE="root@debian-32"
+DEST="prebuilt/armv6l"
+REMOTE="root@pi-black"
 REMOTE_PATH="/root/redmatic-prebuild"
 
-cat addon_files/redmatic/lib/package.json | jq 'del(.dependencies.npm,.dependencies."node-red",.dependencies."@node-red-contrib-themes/midnight-red",.dependencies.node-red-contrib-johnny-five,.dependencies.node-red-contrib-rcswitch2)' > $DEST/package.json
+cat addon_files/redmatic/lib/package.json | jq 'del(.dependencies.npm,.dependencies."node-red",.dependencies."@node-red-contrib-themes/midnight-red")' > $DEST/package.json
 
 scp $DEST/package.json $REMOTE:$REMOTE_PATH
 
 ssh -t $REMOTE "cd $REMOTE_PATH ; npm install --global-style --unsafe-perm --no-package-lock"
 ssh -t $REMOTE "cd $REMOTE_PATH/node_modules/node-red-node-sqlite ; npm install --unsafe-perm --build-from-source --no-package-lock sqlite3"
 
-./prebuild_i686_copy.sh
+./prebuild_armv6l_copy.sh
