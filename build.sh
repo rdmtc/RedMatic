9ccaa72d1a9323aab35108b973cee358d07308b3
hobbyquaker <bastiraff@googlemail.com>
2020-07-31 23:50:24 +0200

    reorder

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ mkdir $BUILD_DIR/dist 2> /dev/null
 echo "installing build dependencies..."
 npm install --only=dev --global-style --no-package-lock
 
-./build_addon.sh armv7l
-./build_addon.sh armv6l
 ./build_addon.sh i686
+./build_addon.sh armv6l
+./build_addon.sh armv7l
 
 ./build_release_body.sh
 ./build_change_history.sh
