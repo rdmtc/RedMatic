1fab52970c697bc8f9a60b39f85890d1c5c83097
hobbyquaker <bastiraff@googlemail.com>
2020-08-08 12:49:36 +0200

    improved dependency update script

---
+++ update_dependencies.js
@@ -0,0 +1,56 @@
+const fs = require('fs').promises;
+const path = require('path');
+const got = require('got');
+const prompts = require('prompts');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const dirs = ['var', 'lib', 'www'];
+
+const pkgs = {
+    combined: require(path.join(__dirname, 'package.json'))
+};
+
+dirs.forEach(dir => {
+    pkgs[dir] = require(path.join(__dirname, 'addon_files/redmatic', dir, 'package.json'));
+    console.log(dir)
+});
+
+async function compareVersions(dir) {
+    const pkgJson = pkgs[dir];
+    const dependencies = pkgJson.dependencies;
+    for (const pkg in dependencies) {
+        const json = await got(`https://registry.npmjs.org/${pkg}`, {
+            responseType: 'json'
+        });
+
+        const actual = dependencies[pkg];
+        const latest = json.body['dist-tags'].latest;
+        if (dependencies[pkg] !== latest) {
+            const response = await prompts({
+                type: 'confirm',
+                name: 'confirmed',
+                message: `update ${pkg} ${dependencies[pkg]} to ${latest}?`
+            });
+            if (response.confirmed) {
+                dependencies[pkg] = latest;
+                pkgs.combined.dependencies[pkg] = `0.0.0 - ${latest}`;
+                const file = path.join('addon_files/redmatic', dir, 'package.json')
+                await fs.writeFile(path.join(__dirname, 'package.json'), JSON.stringify(pkgs.combined, null, '  '));
+                await fs.writeFile(path.join(__dirname, file), JSON.stringify(pkgs[dir], null, '  '));
+                const {stdout, stderr} = await exec(`git commit package.json ${file} -m 'update ${pkg} to ${latest}'`);
+                console.log(stderr, stdout);
+            }
+        }
+    }
+    console.log(pkgs[dir].dependencies, pkgs.combined.dependencies);
+
+}
+async function checkDirs() {
+    for (dir of dirs) {
+        await compareVersions(dir);
+    }
+}
+
+
+checkDirs();
