bb9dccf602c6104814e355350fd8bef6e908d651
hobbyquaker <bastiraff@googlemail.com>
2020-09-05 15:08:44 +0200

    add x86_64

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ mkdir $BUILD_DIR/dist 2> /dev/null
 echo "installing build dependencies..."
 npm install --only=dev --global-style --no-package-lock
 
+./build_addon.sh x86_64
 ./build_addon.sh i686
 ./build_addon.sh armv6l
 ./build_addon.sh armv7l
